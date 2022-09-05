817bbac798bbdbeb2cff5f3403cc826026a9a735
RMagician <mendesdd55@gmail.com>
2022-09-05 22:05:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as fr:
 
 
 setuptools.setup(
-    name="public-hello",
+    name="public_hello",
     version="1.2",
     author="Nicolas REMOND",
     author_email="remondnicola@gmail.com",
